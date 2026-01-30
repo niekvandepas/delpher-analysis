eb329f0a53a7c999f8cadff055e4051e73ca741f
Niek van de Pas <n.vandepas@uu.nl>
2026-01-30 18:40:11 +0100

    feat: get model name from environment rather than hard-coding

--- classifier.py
+++ classifier.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import json
+import os
 from pathlib import Path
 from transformers import pipeline
 import ollama
@@ -13,7 +14,11 @@ classifier = pipeline(
 )
 
 def classify_articles_dutch(texts: list[PlainTextSearchResult]) -> list[OllamaClassificationResult]:
-    MODEL_NAME = 'llama3:8b'
+    # TODO get model name from environment
+    MODEL_NAME = os.environ.get("OLLAMA_CLASSIFICATION_MODEL")
+    if not MODEL_NAME:
+        raise ValueError("Environment variable OLLAMA_CLASSIFICATION_MODEL not set.")
+
     EXAMPLE_TEXT_1 = 'Maastricht – De winst van restaurants loopt terug, maar de populariteit van de buitenlandse keuken neemt in Nederland alleen maar toe. In de periode 1995–2002 is het aantal restaurants met een buitenlandse keuken 19 procent gestegen. De nettowinst voor restaurants is in 10 jaar afgenomen van 11 naar 9 procent in 2001. Dat concludeert het bedrijfschap Horeca en Catering in het sectorrapport Eten in de Nederlandse horeca 2002, dat gisteren werd gepresenteerd tijdens de horecavakbeurs BBB/European Fine Food Fair in Maastricht. Net als de snackbar op de hoek, lijdt ook het restaurant onder de ‘amateurskoks’ die zich – geholpen door de detailhandel – thuis uitsloven. De consument laat vooral de Nederlands-Franse keuken links liggen. Het aantal restaurants met deze keuken is in de afgelopen zeven jaar met 2 procent gedaald. De restaurantsector heeft zijn aandeel in de totale eetomzet binnen de horeca zien afnemen van 49 procent in 1991 naar 44 procent in 2001.'
     EXAMPLE_CATEGORY_1 = "Economie"
     EXAMPLE_TEXT_2 = "Kaas van de markt Eten Wie 's zaterdags zijn kaas koopt op een Hollandse markt, sluit aan in een oude traditie. Wat is er nodig? Een kraam, wat kazen en een mes, een handelaar en een klant - net zoals eeuwen geleden. Onze voorouders spendeerden graag geld aan een brok kaas zoals we uit i7e-eeuwse huishoudboekjes en schuldbekentenissen (!) weten. Ook de export van kaas is stokoud. Reeds voor het jaar 1000 moet er handel zijn geweest, maar het eerste 'harde' bewijs is een document uit 1118, waaruit blijkt dat handelaren uit Stavoren op de Rijn tol betaalden, met boter en kaas, Die kaas kwam veelal uit Noord-Holland en werd over de Zuiderzee naar Friesland gebracht. Een andere route liep via Kampen en Deventer (toen een transitostad van betekenis), vanwaar men de kaas verder over de IJssel verscheepte. Een artistiek bewijs van kaashandel is te zien in de dom van Munster: op een wandschildering prijken boeren uit Friese gewesten die boter en kaas als geschenk aan de Duitse bisschop brengen. Het bereik van de Friese handelaren was opvallend groot: boter, kaas, vet, vee en paarden exporteerden ze naar Haarlem en Alkmaar, maar ook naar Vlaanderen, Denemarken en zelfs naar de Baltische staten en Frankrijk. In de 15e eeuw was de kaashandel - naar huidige maatstaven gerekend - van immense omvang: talloze schippers uit Edam en Enkhuizen voeren schepen vol kaas naar verdere be-stemmingen. Zo stak in de herfst van 1439 schipper Vrederik Hillebrantz uit Edam van wal. Hij voer naar Kampen met 15 vaten boter, 3000 pond kaas en 2500 'deyne kasekens'. Vandaag: een selectie Hollandse 'kasekens' uit het vuistje, met boerenbrood, een zoute haring van de kar en een glas bier. Voor ons een middag- of avondmaal, voor een Gouden Eeuwer een 'banketje' of ontbijt! Alma Huisken"
