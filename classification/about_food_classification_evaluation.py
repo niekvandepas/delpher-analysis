a827591863cfe150b537199082c27a58cda5b548
Niek van de Pas <n.vandepas@uu.nl>
2026-02-03 14:58:23 +0100

    feat: print 'about food' classification metrics from csv file with annotated data

--- classification/about_food_classification_evaluation.py
+++ classification/about_food_classification_evaluation.py
@@ -123,8 +123,10 @@ sample_not_food = not_food_df.sample(n=min(50, len(not_food_df)), random_state=4
 # Combine and shuffle
 eval_set = pd.concat([sample_food, sample_not_food]).sample(frac=1, random_state=42)
 
-annotated_df = annotate_entries_curses(eval_set)
-print_metrics(annotated_df)
-
 annotated_data_path = Path(ANNOTATED_DATA_DIR) / "is_about_food_annotated.csv"
-annotated_df.to_csv(annotated_data_path, index=False)
+
+# annotated_df = annotate_entries_curses(eval_set)
+# annotated_df.to_csv(annotated_data_path, index=False)
+
+annotated_df = pd.read_csv(annotated_data_path)
+print_metrics(annotated_df)
